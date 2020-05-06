1057f9a8cb804ffa71d1b6483b4e5e9da41bdad2
poolitzer <25934244+poolitzer@users.noreply.github.com>
2020-05-05 20:40:29 -0700

    fix: add method to init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .methods import get_next_question
+from .methods import get_next_question, get_user_scores
 
-__all__ = ["get_next_question"]
+__all__ = ["get_next_question", "get_user_scores"]
