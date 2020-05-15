dd4db0ff1c0de3d583ca9d7bbc18ee2055196fac
poolitzer <25934244+poolitzer@users.noreply.github.com>
2020-05-14 20:08:58 -0700

    fix: checking for previous question if the question is the first one
    also reverting andrews changes smh

--- methods.py
+++ methods.py
@@ -111,9 +111,9 @@ def get_user_scores(user_id):
 
 def get_previous_question(user_id, lang, question_id):
     index = get_question_index(question_id)
-    previous_question_id = get_question_id_from_index(index - 1)
-    if not previous_question_id:
+    if index == 0:
         return None
+    previous_question_id = get_question_id_from_index(index - 1)
     question = InternQuestion(previous_question_id, **get_question(previous_question_id))
     if question.guard:
         if not _test_question(question, user_id):
